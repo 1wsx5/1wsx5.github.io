823afc18cea813342c23120747c8805b9986f2f6
1wsx5 <55280927+1wsx5@users.noreply.github.com>
2022-10-08 17:49:55 -0400

    Update Click.js

--- Cook1e Click1r/Click.js
+++ Cook1e Click1r/Click.js
@@ -1,6 +1,21 @@
 var clicks = 0;
-
+var x = 0
 function onClick() {
   clicks += 1;
   document.getElementById("clicks").innerHTML = clicks;
 };
+function SteamClick(){
+if (clicks == 50 && document.getElementById("steampunk").onclick) {
+  ;
+  clicks -= 50;
+  x += 1;
+  }
+};
+// Will execute myCallback every 0.5 seconds 
+var intervalID = setInterval(myCallback, x*1000);
+
+function myCallback() {
+  clicks += 1;
+}
+console.log(x);
+console.log(clicks);
